61bfb48839b7dd725e9627299042a244eaa4191a
yousuf khann <mykhann10@gmail.com>
2025-02-16 14:54:27 +0500

    added cookie parser for login

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ import express from "express";
 import "dotenv/config";
 import connectToDb from "./src/Database/db.js"; 
 import userRouter from "./src/routes/user.routes.js"
+import cookieParser from "cookie-parser";
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.use("/api/v1/user",userRouter)
 
