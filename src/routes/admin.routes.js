33106406f5d593453fa73d89a6029437b72611bf
yousuf khann <mykhann10@gmail.com>
2025-02-17 20:46:21 +0500

    added view all users to admin controller

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { viewAllBookings, viewAllRooms } from "../controllers/admin.controller.js";
+import { viewAllBookings, viewAllRooms, viewAllUsers } from "../controllers/admin.controller.js";
 import { isAuthenticated } from "../Middleware/isAuthenticated.middleware.js";
 const router= new Router();
 
 router.get("/bookings",isAuthenticated,viewAllBookings)
 router.get("/rooms",isAuthenticated,viewAllRooms)
+router.get("/users",isAuthenticated,viewAllUsers)
 
 
 
